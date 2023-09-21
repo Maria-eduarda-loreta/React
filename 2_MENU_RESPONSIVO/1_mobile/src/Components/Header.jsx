d6584d75d32e5e0aedae78e00cd222b6b2df7c59
maria <eduardaloreta2206@gmail.com>
2023-09-21 14:08:07 -0300

    ícone ,  abrir e fechar

--- 2_MENU_RESPONSIVO/1_mobile/src/Components/Header.jsx
+++ 2_MENU_RESPONSIVO/1_mobile/src/Components/Header.jsx
@@ -1,24 +1,46 @@
-import { List } from "@phosphor-icons/react"
+import { List, X } from "@phosphor-icons/react"
+import { useState } from "react"
+
 import styles from "./Header.module.css"
 
 
 function Header() {
+    const [abrirMenu, setAbrirMenu] = useState(false)
+
+    function handleAbrirFecharMenu() {
+        if (abrirMenu === true) {
+            setAbrirMenu(false)
+            return
+        }
+
+        setAbrirMenu(true)
+    }
+
   return (
     <header className={styles.header}>
         <div className={styles.mobile}>
             <div className={styles.menu}>
-                <button>
-                    <List size={32} />
-                </button>    
+                <button onClick={handleAbrirFecharMenu}>
+                   {
+                    abrirMenu === true ? (
+                        <X size={32} />
+                    ) : (
+                        <List size={32} />
+                    )
+                   }
+                </button> 
+
                 <h1>Logo</h1>      
             </div>
-            <div className={styles.options}></div>
-            <nav>
-                <a href="">Home</a>
-                <a href="">Resultados</a>
-                <a href="">Produtos</a>
-                <a href="">Login</a>
-            </nav>
+
+            <div className={`${styles.options} ${abrirMenu === true && styles.open}`}>
+                <nav>
+                    <a href="">Home</a>
+                    <a href="">Resultados</a>
+                    <a href="">Produtos</a>
+                    <a href="">Login</a>
+                </nav>
+            </div>
         </div>
     </header>
   )
